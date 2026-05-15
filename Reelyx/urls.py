5d1d751663992d4f12450ebe4820c55e3e717759
samanthasofiaparrariera-bit <samantha.sofia.parra.riera@estudiante.epsum.school>
2026-05-15 21:50:20 +0200

    Add backend home page

--- Reelyx/urls.py
+++ Reelyx/urls.py
@@ -1,5 +1,5 @@
 
-
+from django.http import HttpResponse
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
@@ -9,7 +9,19 @@ from django.conf import settings
 admin.site.site_header = "Reelyx"
 admin.site.index_title = "App Películas"
 
+def home(request):
+    return HttpResponse("""
+    <html>
+      <body style="background:#151315; color:white; font-family:Arial; text-align:center; padding:60px;">
+        <h1>Reelyx API</h1>
+        <p>Backend desplegado correctamente.</p>
+        <a style="color:#bb81d9;" href="/api/peliculas/">Ver películas</a>
+      </body>
+    </html>
+    """)
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 
     path('api/users/', include('Users.urls')),
